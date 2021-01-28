3f2d1af2c087e6a8650d7ee97c6bde13240c12c3
Brandon Ha <hsy0602@outlook.com>
2021-01-28 16:46:23 +0900

    putting header components

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,15 @@
-import { Nav, NavContainer, NavLogo } from "./HeaderElements";
+import {
+  Nav,
+  NavContainer,
+  NavLogo,
+  NavLinks,
+  NavItem,
+  NavLink,
+  NavAccount,
+  NavUser,
+  SignIn,
+  SignUp,
+} from "./HeaderElements";
 
 const Header = () => {
   return (
@@ -6,6 +17,25 @@ const Header = () => {
       <Nav>
         <NavContainer>
           <NavLogo to="/">Tesla</NavLogo>
+          <NavLinks>
+            <NavItem>
+              <NavLink to="rent">Rent</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="yourcar">Your Car</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="discover">Discover</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="safety">Safety</NavLink>
+            </NavItem>
+          </NavLinks>
+          <NavAccount>
+            <NavUser>UserIcon</NavUser>
+            <SignIn>Sign In</SignIn>
+            <SignUp>Sign Up</SignUp>
+          </NavAccount>
         </NavContainer>
       </Nav>
     </header>
